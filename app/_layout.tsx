41286122dfd7f2d5dc7477846de62b2c260f4c2d
Jon Samp <sampjon@gmail.com>
2026-04-20 21:08:56 -0500

    updates to expo-observe

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import AppMetrics from "expo-eas-observe";
+import { AppMetrics } from "expo-observe";
 import "../global.css";
 
 export default function Layout() {
@@ -12,54 +12,54 @@ export default function Layout() {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-    <SafeAreaProvider>
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          title: "Scanner",
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="logs"
-        options={{
-          title: "Logs",
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="about"
-        options={{
-          title: "About",
-          headerShown: false,
-          presentation: "modal",
-        }}
-      />
-      <Stack.Screen
-        name="generate-qr"
-        options={{
-          title: "Generate QR",
-          headerShown: false,
-          presentation: "modal",
-        }}
-      />
-      <Stack.Screen
-        name="home"
-        options={{
-          title: "QRU?",
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="privacy"
-        options={{
-          title: "Privacy Policy",
-          headerShown: false,
-        }}
-      />
-    </Stack>
-    </SafeAreaProvider>
+      <SafeAreaProvider>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "Scanner",
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="logs"
+            options={{
+              title: "Logs",
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="about"
+            options={{
+              title: "About",
+              headerShown: false,
+              presentation: "modal",
+            }}
+          />
+          <Stack.Screen
+            name="generate-qr"
+            options={{
+              title: "Generate QR",
+              headerShown: false,
+              presentation: "modal",
+            }}
+          />
+          <Stack.Screen
+            name="home"
+            options={{
+              title: "QRU?",
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="privacy"
+            options={{
+              title: "Privacy Policy",
+              headerShown: false,
+            }}
+          />
+        </Stack>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
